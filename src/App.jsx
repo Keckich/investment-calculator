9708120a8cabf592283963b948640a632b5568ca
Keckich <nik.malashko@mail.ru>
2023-11-25 22:31:03 +0100

    added InputData component and a container for input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Header from "./util/components/Header";
+import InputData from "./util/components/InputData";
 
 function App() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <section id="user-input">
+        <div className="input-group">
+          <InputData title="Initial investment" />
+          <InputData title="Annual investment" />
+        </div>
+        <div className="input-group">
+          <InputData title="Expected return" />
+          <InputData title="Duration" />
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/util/components/InputData.jsx
@@ -0,0 +1,8 @@
+export default function InputData({ title }) {
+  return (
+    <div className="">
+      <label>{title}</label>
+      <input type="number" />
+    </div>
+  );
+}
